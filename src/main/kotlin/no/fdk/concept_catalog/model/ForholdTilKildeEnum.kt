ca9e47a1827505ca350fb987fb3b62cdffc4b6aa
NilsOveTen <github.nils.ove@tendenes.com>
2021-05-26 13:07:49 +0200

    fix: deserialize enum as value

--- src/main/kotlin/no/fdk/concept_catalog/model/ForholdTilKildeEnum.kt
+++ src/main/kotlin/no/fdk/concept_catalog/model/ForholdTilKildeEnum.kt
@@ -1,7 +1,12 @@
 package no.fdk.concept_catalog.model
 
+import com.fasterxml.jackson.annotation.JsonValue
+
 enum class ForholdTilKildeEnum(val value: String) {
     EGENDEFINERT("egendefinert"),
     BASERTPAAKILDE("basertPaaKilde"),
-    SITATFRAKILDE("sitatFraKilde")
+    SITATFRAKILDE("sitatFraKilde");
+
+    @JsonValue
+    fun jsonValue(): String = value
 }
