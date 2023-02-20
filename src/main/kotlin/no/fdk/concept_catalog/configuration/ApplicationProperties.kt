d86e376de7cfc33ff81d2b1831ae621254e832ac
NilsOveTen <github.nils.ove@tendenes.com>
2023-02-20 14:03:28 +0100

    chore: upgrade spring boot

--- src/main/kotlin/no/fdk/concept_catalog/configuration/ApplicationProperties.kt
+++ src/main/kotlin/no/fdk/concept_catalog/configuration/ApplicationProperties.kt
@@ -1,9 +1,7 @@
 package no.fdk.concept_catalog.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
-@ConstructorBinding
 @ConfigurationProperties("application")
 data class ApplicationProperties(
     val collectionBaseUri: String
