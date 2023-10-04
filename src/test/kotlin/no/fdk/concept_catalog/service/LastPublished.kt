67ebecce14b8785699b0640d967d074876272d59
NilsOveTen <github.nils.ove@tendenes.com>
2023-10-04 12:30:39 +0200

    chore: all updates of concepts in mongo is replicated in elastic

--- src/test/kotlin/no/fdk/concept_catalog/service/LastPublished.kt
+++ src/test/kotlin/no/fdk/concept_catalog/service/LastPublished.kt
@@ -2,6 +2,7 @@ package no.fdk.concept_catalog.service
 
 import no.fdk.concept_catalog.configuration.ApplicationProperties
 import no.fdk.concept_catalog.configuration.JacksonConfigurer
+import no.fdk.concept_catalog.elastic.ConceptSearchRepository
 import no.fdk.concept_catalog.model.SemVer
 import no.fdk.concept_catalog.model.Status
 import no.fdk.concept_catalog.repository.ConceptRepository
@@ -22,13 +23,14 @@ import kotlin.test.assertTrue
 class LastPublished {
     private val conceptRepository: ConceptRepository = mock()
     private val conceptSearch: ConceptSearchService = mock()
+    private val conceptSearchRepository: ConceptSearchRepository = mock()
     private val mongoOperations: MongoOperations = mock()
     private val applicationProperties: ApplicationProperties = mock()
     private val conceptPublisher: ConceptPublisher = mock()
     private val historyService: HistoryService = mock()
 
     private val conceptService = ConceptService(
-        conceptRepository, conceptSearch, mongoOperations, applicationProperties, conceptPublisher, historyService, JacksonConfigurer().objectMapper())
+        conceptRepository, conceptSearch, conceptSearchRepository, mongoOperations, applicationProperties, conceptPublisher, historyService, JacksonConfigurer().objectMapper())
 
     @Test
     fun `Able to get a list with the highest version of concepts for a publisher`() {
