a03cf58d88dfd78ea0fe732a8c31ab2c0f6dc9f9
hegeaal <hegeaal@gmail.com>
2024-05-28 14:41:22 +0200

    chore: update assosiative relation to skos v2

--- src/main/kotlin/no/fdk/concept_catalog/rdf/SKOSNO.kt
+++ src/main/kotlin/no/fdk/concept_catalog/rdf/SKOSNO.kt
@@ -10,7 +10,7 @@ class SKOSNO {
 
         val Definisjon: Resource = ResourceFactory.createResource("${uri}Definisjon")
         val AlternativFormulering: Resource = ResourceFactory.createResource("${uri}AlternativFormulering")
-        val AssosiativRelasjon: Resource = ResourceFactory.createResource("${uri}AssosiativRelasjon")
+        val AssociativeConceptRelation: Resource = ResourceFactory.createResource("${uri}AssociativeConceptRelation")
         val GeneriskRelasjon: Resource = ResourceFactory.createResource("${uri}GeneriskRelasjon")
         val PartitiveConceptRelation: Resource = ResourceFactory.createResource("${uri}PartitiveConceptRelation")
 
@@ -21,12 +21,13 @@ class SKOSNO {
         val relationshipWithSource: Property = ResourceFactory.createProperty( "${uri}relationshipWithSource")
         val valueRange: Property = ResourceFactory.createProperty( "${uri}valueRange")
         val datastrukturterm: Property = ResourceFactory.createProperty( "${uri}datastrukturterm")
-        val assosiativRelasjon: Property = ResourceFactory.createProperty( "${uri}assosiativRelasjon")
+        val isFromConceptIn: Property = ResourceFactory.createProperty( "${uri}isFromConceptIn")
         val hasPartitiveConceptRelation: Property = ResourceFactory.createProperty( "${uri}hasPartitiveConceptRelation")
         val generiskRelasjon: Property = ResourceFactory.createProperty( "${uri}generiskRelasjon")
         val inndelingskriterium: Property = ResourceFactory.createProperty( "${uri}inndelingskriterium")
         val hasPartitiveConcept: Property = ResourceFactory.createProperty("${uri}hasPartitiveConcept")
         val hasComprehensiveConcept: Property = ResourceFactory.createProperty("${uri}hasComprehensiveConcept")
+        val hasToConcept: Property = ResourceFactory.createProperty("${uri}hasToConcept")
 
         val allmennheten: Property = ResourceFactory.createProperty( "${uri}allmennheten")
         val fagspesialist: Property = ResourceFactory.createProperty( "${uri}fagspesialist")
