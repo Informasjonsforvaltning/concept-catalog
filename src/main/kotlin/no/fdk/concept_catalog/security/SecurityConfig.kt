c9c98b13fc4c8d817c8d0fbd5a9102d9957be102
Nils Ove Tendenes <nils.ove@tendenes.com>
2024-06-25 11:08:13 +0200

    Revert "feat: add csp header"
    This reverts commit f22ee4ea3e879296959adef6d8ea4ab5970ea946.

--- src/main/kotlin/no/fdk/concept_catalog/security/SecurityConfig.kt
+++ src/main/kotlin/no/fdk/concept_catalog/security/SecurityConfig.kt
@@ -6,34 +6,19 @@ import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2Res
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.header.writers.StaticHeadersWriter
 import org.springframework.security.web.util.matcher.RequestMatcher
-import org.springframework.web.cors.CorsConfiguration
 
 @Configuration
 open class SecurityConfig {
 
     @Bean
     open fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf { csrf -> csrf.disable() }
-            .headers { headers ->
-                headers.addHeaderWriter(
-                    StaticHeadersWriter(
-                        "Content-Security-Policy-Report-Only", "default-src 'self' fellesdatakatalog.digdir.no *.fellesdatakatalog.digdir.no"
-                    )
-                )
-                headers.addHeaderWriter(
-                    StaticHeadersWriter(
-                        "Reporting-Endpoints", "europe-west1-digdir-cloud-functions.cloudfunctions.net"
-                    )
-                )
-            }
-            .cors { cors -> cors.configurationSource { CorsConfiguration().applyPermitDefaultValues() } }
+        http.csrf().disable()
+            .cors().and()
             .authorizeHttpRequests{ authorize ->
                 authorize.requestMatchers(RDFMatcher()).permitAll()
                     .requestMatchers(HttpMethod.OPTIONS).permitAll()
@@ -41,7 +26,7 @@ open class SecurityConfig {
                     .requestMatchers(HttpMethod.GET,"/ready").permitAll()
                     .requestMatchers(HttpMethod.GET,"/actuator/**").permitAll()
                     .anyRequest().authenticated() }
-            .oauth2ResourceServer { resourceServer -> resourceServer.jwt(Customizer.withDefaults()) }
+            .oauth2ResourceServer { resourceServer -> resourceServer.jwt() }
         return http.build()
     }
 
