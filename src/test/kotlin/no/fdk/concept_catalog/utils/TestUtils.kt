d87da7cccd6eb5ca054e928ff3248eac8ca6958f
Nils Ove Tendenes <nils.ove@tendenes.com>
2024-12-02 07:49:03 +0100

    Reapply "refactor: rm extra info in DTO about last published version when not search result"
    This reverts commit 1e445cc2b4360d3126f88de017d68b5391c4a10c.

--- src/test/kotlin/no/fdk/concept_catalog/utils/TestUtils.kt
+++ src/test/kotlin/no/fdk/concept_catalog/utils/TestUtils.kt
@@ -311,8 +311,4 @@ fun checkIfIsomorphicAndPrintDiff(actual: Model, expected: Model, name: String, 
     return isIsomorphic
 }
 
-fun Begrep.fromSearch() = copy(
-    erSistPublisert = false,
-    revisjonAvSistPublisert = false,
-    gjeldendeRevisjon = null
-)
+fun Begrep.fromDBO() = copy(sistPublisertId = null)
