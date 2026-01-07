c497c483986f7a0a31b1f3f58951a6b6a33d913b
Hassan Alam <hassan@hotmail.no>
2026-01-07 13:44:11 +0100

    refactor: Spring Boot 4 uses the Elasticsearch Java Client which no longer relies on Apache HttpClient.

--- src/main/kotlin/no/fdk/concept_catalog/elastic/ElasticsearchConfig.kt
+++ src/main/kotlin/no/fdk/concept_catalog/elastic/ElasticsearchConfig.kt
@@ -1,28 +1,31 @@
 package no.fdk.concept_catalog.elastic
 
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy
-import org.apache.http.ssl.SSLContextBuilder
-import org.apache.http.ssl.SSLContexts
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.client.ClientConfiguration
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration
 import java.io.File
+import java.io.FileInputStream
+import java.security.KeyStore
 import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 @Configuration
 open class ElasticsearchConfig(private val elasticProperties: ElasticProperties): ElasticsearchConfiguration() {
 
     private fun sslContext(): SSLContext {
-        val builder: SSLContextBuilder = SSLContexts.custom()
+        val trustStore = KeyStore.getInstance(KeyStore.getDefaultType())
+        FileInputStream(File(elasticProperties.storePath)).use { inputStream ->
+            trustStore.load(inputStream, elasticProperties.storePass.toCharArray())
+        }
 
-        builder.loadTrustMaterial(
-            File(elasticProperties.storePath),
-            elasticProperties.storePass.toCharArray(),
-            TrustSelfSignedStrategy()
-        )
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(trustStore)
 
-        return builder.build()
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, trustManagerFactory.trustManagers, null)
+        
+        return sslContext
     }
 
     @Bean(name = ["elasticsearchClientConfiguration"])
@@ -30,7 +33,9 @@ open class ElasticsearchConfig(private val elasticProperties: ElasticProperties)
         val builder = ClientConfiguration.builder()
             .connectedTo(elasticProperties.host)
 
-        if (elasticProperties.ssl) builder.usingSsl(sslContext())
+        if (elasticProperties.ssl) {
+            builder.usingSsl(sslContext())
+        }
 
         builder.withBasicAuth(
             elasticProperties.username,
