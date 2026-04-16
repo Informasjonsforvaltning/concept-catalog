6dfe9a5a5e515ae900ed96f687104735a3423cbe
Nils Ove Tendenes <nils.ove@tendenes.com>
2026-04-16 13:34:16 +0200

    fix: handle null on int fields with default value

--- src/main/kotlin/no/fdk/concept_catalog/model/Pagination.kt
+++ src/main/kotlin/no/fdk/concept_catalog/model/Pagination.kt
@@ -1,11 +1,15 @@
 package no.fdk.concept_catalog.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Pagination (
-    private val page: Int = 0,
-    private val size: Int = 10
+    @param:JsonProperty("page", required = false)
+    private val page: Int? = null,
+    @param:JsonProperty("size", required = false)
+    private val size: Int? = null
 ) {
-    fun getPage(): Int = page.let { if (it < 0) 0 else it }
-    fun getSize(): Int = size.let { if (it < 1) 10 else it }
+    fun getPage(): Int = page.let { if (it == null || it < 0) 0 else it }
+    fun getSize(): Int = size.let { if (it == null || it < 1) 10 else it }
 }
 
 data class Paginated (
